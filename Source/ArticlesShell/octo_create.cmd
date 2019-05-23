fab66a8df045c17b3466f5bbb30292aa035b2224
Petrov Maksim <maksim.petrov@infotecs.ru>
2019-05-23 15:17:26 +0300

    тестовый коммит

---
+++ Source/ArticlesShell/octo_create.cmd
@@ -0,0 +1,3 @@
+octo pack ./Sources/ArticlesShell --id="ArticlesService" --version="1.1.0" --outFolder="../../out/pack"
+octo pack --id="ArticlesShell" --version="1.1.0" --outFolder="../../out/pack"
+octo pack --id="ArticlesWeb" --version="1.1.0" --outFolder="../../out/pack"
